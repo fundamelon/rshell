583032a145e1a5dbf19eb98d6de74f49c2d4b056
fundamelon <igi.c@att.net>
2015-04-30 17:50:01 -0700

    recursive function, formatting
    + added full recursion along with proper formatting for all files
    
    KNOWN BUGS: sometimes attempts to invoke itself on empty file, crashes

--- src/ls.cpp
+++ src/ls.cpp
@@ -9,6 +9,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 
 #include <iostream>
 #include <vector>
@@ -83,47 +84,68 @@ bool namecmp(std::string i, std::string j) {
 // take a filepath and print out its contents
 void readloc(const char* path) {
 
-//    std::cout << "path: " << path << std::endl;
-
-    struct stat stat_buf;
-    if(stat(path, &stat_buf) == -1) { perror("stat"); exit(1); }
-
-    if(S_ISDIR(stat_buf.st_mode)) {
-        if(LS_MODE & LS_MODE_MANYFILES) std::cout << path << ":" << std::endl;
-    } else {
-        std::cout << path << std::endl;
-        return;
-    }
+    std::cout << "DEBUG: " << path << std::endl;
+    if(     LS_MODE & LS_MODE_MANYFILES ||
+            LS_MODE & LS_MODE_RECURSIVE     ) 
+        std::cout << path << ":" << std::endl;
 
     // get and sort files within directory
     auto files = scandir(path);
     std::sort(files.begin(), files.end(), namecmp);
 
-    for(auto f : files) {
-//        std::cout << "file: " << f << std::endl;
-        if(LS_MODE & LS_MODE_SHOWALL || f[0] != '.') {
-            if(LS_MODE & LS_MODE_RECURSIVE) 
-                readloc((std::string(path) + "/" + f).c_str());
-            else
-                std::cout << f << "  ";
-        } else continue;
+    // create container of same type for dirs
+    decltype(files) dirs;
 
+    for(auto f : files) {
+        
+        const char* fullpath = (std::string(path) + "/" + f).c_str();
+
+
+        //stat the file
+        struct stat stat_buf;
+        if(stat(fullpath, &stat_buf) == -1) { perror("stat"); exit(1); }
+       
+        // skip hidden files if not in showall mode
+        if(f == "." || f == ".." || (!(LS_MODE & LS_MODE_SHOWALL) && f[0] == '.'))
+            continue; 
+        
+        // handle as dir or print filename
+        if(S_ISDIR(stat_buf.st_mode) && (LS_MODE & LS_MODE_RECURSIVE)) {
+//          files.erase(std::remove(files.begin(), files.end(), f), files.end());
+            dirs.push_back(f);
+        }
     }
 
+    for(auto f : files)
+        if(LS_MODE & LS_MODE_SHOWALL ||  f[0] != '.')
+            std::cout << f << "  ";
+        
     std::cout << std::endl;
+
+    if(LS_MODE & LS_MODE_RECURSIVE) 
+        for(auto d : dirs)
+            readloc((std::string(path) + "/" + d).c_str());
 }
 
 
 // scan a directory for all contained files
 std::vector<std::string> scandir(const char* path) {
    
+    std::vector<std::string> files;
+
+    struct stat stat_buf;
+    if(stat(path, &stat_buf) == -1) { perror("stat"); exit(1); }
+    if(!S_ISDIR(stat_buf.st_mode)) {
+//        files.push_back(path);
+        return files;
+    }
+
 	DIR* dirp;
     if( (dirp = opendir(path)) == NULL) {
         perror("opendir");
         exit(1);
     }
 
-    std::vector<std::string> files;
 
     struct dirent* entry;
     errno = 0;
